722d3a6c888f1bbb33bc4f9cb5bcc2486b898dc0
Ntwali O. Axel <132359348+execxel@users.noreply.github.com>
2026-06-29 20:35:20 +0200

    Initial commit

---
+++ quest_9_the_greedy_goblin.py
@@ -0,0 +1,9 @@
+#The Greedy Goblin
+
+total_gold = 27
+
+shared_gold = total_gold // 4
+left_gold = total_gold % 4
+
+print("The greedy goblin has", left_gold, "gold coins left and.")
+print("He shares", shared_gold, "gold coins with his 4 friends.")
