b1c399323f0ccb155a7bdb09d62c918d5fbf157c
Gwiza Tysha Kansiime <g.kansiime@alustudent.com>
2026-06-29 01:56:30 -0700

    Add Level 3: Crossroads of Logic

---
+++ quest12.py
@@ -0,0 +1,5 @@
+password = input("Enter the password: ")
+if password == "open sesame":
+    print("Access Granted")
+else:
+    print("Access Denied")
---
+++ quest14.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age: "))
+gold = int(input("Enter your gold coins: "))
+if age >= 18 and gold >= 20:
+    print("Welcome! You may enter the club.")
+else:
+    print("Sorry, you cannot enter.")
