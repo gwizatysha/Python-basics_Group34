30cb3b4618d2da74c21bd693c71e80f74ba08154
Abdikarim <a.mohamed1@alustudent.com>
2026-06-29 08:14:34 -0700

    Added quest 02 naming ceremony and quest 03 treasure chest

---
+++ quest_02_naming_ceremony.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Quest 2 - The Naming Ceremony
+hero_name = "Arthur"
+superpower = "invisibility"
+
+print(f"This is {hero_name} and their superpower is {superpower}!")
---
+++ quest_03_treasure_chest.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Quest 3 - The Treasure Chest
+age = 21
+favorite_food = "pizza"
+
+print(f"I am {age} years old and my favorite food is {favorite_food}.")
