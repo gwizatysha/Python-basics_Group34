7ab8cc9c75f7f18db6a4f0756c08f26aad84711f
imutabazi <i.mutabazi@alustudent.com>
2026-06-29 22:35:05 +0200

    add comments

--- quest_27_the_fizzbuzz_test.py
+++ quest_27_the_fizzbuzz_test.py
@@ -1,9 +1,18 @@
 for number in range(1, 101):
+    
+    # If the number is divisible by both 3 and 5 print FizzBuzz
+    # We check this first before checking 3 or 5 alone
     if number % 3 == 0 and number % 5 == 0:
         print("FizzBuzz")
+
+  # If the number is only divisible by 3 print Fizz
     elif number % 3 == 0:
         print("Fizz")
+    
+    # If the number is only divisible by 5 print Buzz
     elif number % 5 == 0:
         print("Buzz")
+
+    # If none of the above just print the number itself
     else:
         print(number)
