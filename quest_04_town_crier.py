c70c0007a293f2e0a3fb90fb1a30ce9cb29d8313
Abdikarim <a.mohamed1@alustudent.com>
2026-06-29 08:18:19 -0700

    Added quest 04 town crier

---
+++ quest_04_town_crier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Quest 4 - The Town Crier
+city_name = "Kigali"
+current_year = 2026
+your_name = "Arthur"
+
+print(f"Welcome to {city_name}! The year is {current_year}, and our newest resident is {your_name}.")
