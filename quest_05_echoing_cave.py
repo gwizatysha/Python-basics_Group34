39b875d0aa2cda5d89cda67208e9a624f4fa6fb7
Abdikarim <a.mohamed1@alustudent.com>
2026-06-29 08:20:03 -0700

    Added quest 05 echoing cave

---
+++ quest_05_echoing_cave.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Quest 5 - The Echoing Cave
+player_health = 100
+print(f"Starting health: {player_health}")
+
+player_health = player_health - 25
+print(f"After monster attack: {player_health}")
+
+player_health = player_health + 10
+print(f"After drinking potion: {player_health}")
